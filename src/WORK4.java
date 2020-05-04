d9364f99e7127f99173767eb4e1fc97062543721
haileeey <1248214831@qq.com>
2020-05-04 05:14:25 -0400

    finish work4and5

--- src/WORK4.java
+++ src/WORK4.java
@@ -1,7 +1,22 @@
+// the array start with 10 capacity, as user type 8 inputs, the capacity will automatically extends to
+// 15 (1.5 times of original capacity), then if the user types 4 more inputs(12 intotal), the capacity
+// will extends to 22(1.5*15), and so on
+import java.util.Scanner;
 public class WORK4 {
     public static void main(String[] args){
-        System.out.println("");
-
+        int capacity =0;
+        int[] arr1 = new int[10];
+        do{
+            System.out.println("current capacity for array is "+arr1.length);
+            for(int i=0; i< ((arr1.length-capacity)*4/5); i++){
+                System.out.println("输入一个值:");
+                Scanner sc = new Scanner(System.in);
+                int tempt = sc.nextInt();
+            }
+            capacity = arr1.length;
+            int newsize =  arr1.length*3/2;
+            arr1 = new int[newsize];
+        }while(true);
 
     }
 }
--- src/WORK5.java
+++ src/WORK5.java
@@ -1,6 +1,21 @@
+import java.util.Arrays;
 public class WORK5 {
     public static void main(String[] args){
-        int[] arr1 = {0,1,2,3,4,5,6,7,8,9};
-        char[] arr2 = {a,b,c,d,e,f};
+        char[][] arr2d = new char[17][17];
+        char[] tempt = {32,48,49,50,51,52,53,54,55,56,57,97,98,99,100,101,102};
+        arr2d[0] = tempt;
+        for(int i=1; i<=16; i++){
+            arr2d[i][0] = tempt[i];
+            for(int j=1; j<=16; j++) {
+                arr2d[i][j] =43;
+            }
+        }
+        for(int j=0; j<17; j++){
+            for(int k=0; k<17; k++){
+                System.out.print(arr2d[j][k]+" ");
+            }
+            System.out.println();
+        }
+
     }
 }
